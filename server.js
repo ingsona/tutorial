86803708d74409cf82ee9b71ea4dcb7d3b5075a6
ingsona <ingsona06@gmail.com>
2021-01-01 15:39:59 +0700

    make routes

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to bezkoder application." });
 });
+require("./app/routes/turorial.routes")(app);
 
 //set port
 const PORT = process.env.PORT || 8080;
